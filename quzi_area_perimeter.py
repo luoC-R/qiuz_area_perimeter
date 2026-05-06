01a861333de8d3c026d854c655b6e2842e946578
luo <chenl24889@masseyhigh.school.nz>
2026-05-06 13:01:46 +1200

    fix my code

--- quzi_area_perimeter.py
+++ quzi_area_perimeter.py
@@ -25,64 +25,10 @@ def string_checker(question, valid_ans=("yes", "no")):
        print(error)
        print()
 
-def calculate_type_checker(question,valid_ans=("perimeter", "area")):
-    error = f"Please enter s valid option from the following list: {valid_ans}"
-
-    while True:
-
-        # Get user response and make sure it's lowercase
-        user_response = input(question).lower()
-
-        for item in valid_ans:
-            # check if the user response is a word in the list
-            if item == user_response:
-                return item
-
-            # check if the user response is the same as
-            # the first letter of an item in the list
-            elif user_response == item[0]:
-                return item
-
-        # print error if user does not enter something that is valid
-        print(error)
-        print()
-
-# Ask user for width and loop until they
-# enter a number that is more than zero
-def num_check(question):
-
-    error = "please enter a number that is more than zero\n"
-    while True:
-
-        try:
-            # ask the user for a number
-            response = float(input(question))
-
-            #check that the number is more than zero
-            if 50 > response > 0:
-               return  response
-            else:
-                print(error)
-        except ValueError:
-            print(error)
 
 def number(low, high):
     return random.randint(low, high)
 
-def yes_no(question):
-    """Checks user response to a question is yes / no (y/n), returns 'yes' or 'no' """
-
-    while True:
-
-        response = input(question).lower()
-
-        # check the user says yes / no / y / n
-        if response == "yes" or response == "y":
-            return "yes"
-        elif response == "no" or response == "n":
-            return "no"
-        else:
-            print("please enter yes / no")
 
 def check_answer(user_answer, correct_answer):
     if abs(user_answer - correct_answer ) < 0.01:
@@ -107,13 +53,12 @@ def instructions():
 
 # Main routine Starts here
 
-all_scores = []
 feedback = ""
 choose = 1
 
-
-shape_list = ["triangle", "square", "rectangle", "xxx"]
-game_history = []
+formula_list = ["perimeter", "area"]
+shape_list = ["triangle", "square", "rectangle",]
+quiz_history = []
 random_number1 = number(5, 30)
 random_number2 = number(5, 30)
 random_number3 = number(1,20)
@@ -132,12 +77,13 @@ if want_instructions == "yes":
 
 
 while True:
-    selected_graph = string_checker("Which graphic do you want to learn about?", shape_list)
+    selected_graph = string_checker("Which shape do you want to learn about?", shape_list)
     print("You choose", selected_graph)
     print()
-    formula = calculate_type_checker("you want to calculation perimeter or area ?")
+    formula = string_checker("you want to calculation perimeter or area ?", formula_list)
     print("You choose", formula)
 
+    # the shape is not same so use if to use different formula
 
     if selected_graph == "square":
         user_answer = float(input(f"What is the {formula} of a square that has a side of {random_number1}? "))
@@ -147,7 +93,7 @@ while True:
             feedback = "correct" if is_correct else "wrong"
 
         elif formula == "area":
-            correct_answer = random_number1 ** 2
+            correct_answer = random_number1 * random_number1
             is_correct = check_answer(user_answer, correct_answer)
             feedback = "correct" if is_correct else "wrong"
 
@@ -156,6 +102,7 @@ while True:
     elif selected_graph == "triangle":
         if formula == "perimeter":
             user_answer = float(input(f"What is the {formula} of a triangle that has sides {random_number1}, {random_number2}, {random_number3}? "))
+            # check user answer is correct
             correct_answer = random_number1 + random_number2 + random_number3
             is_correct = check_answer(user_answer, correct_answer)
             feedback = "correct" if is_correct else "wrong"
@@ -167,39 +114,41 @@ while True:
             feedback = "correct" if is_correct else "wrong"
 
     elif selected_graph == "rectangle":
+        user_answer = float(input(f"What is the {formula} of a rectangle with width {random_number1} and height {random_number2}? "))
+
         if formula == "perimeter":
             correct_answer = 2 * (random_number1 + random_number2)
-            user_answer = float(input(f"What is the {formula} of a rectangle with width {random_number1} and height {random_number2}? "))
             is_correct = check_answer(user_answer, correct_answer)
+
+            #Define feedback to ensure there is a correct value in the history.
             feedback = "correct" if is_correct else "wrong"
 
 
 
         elif formula == "area":
             correct_answer = random_number1 * random_number2
-            user_answer = float(input(f"What is the {formula} of a rectangle with width {random_number1} and height {random_number2}? "))
-
             is_correct = check_answer(user_answer, correct_answer)
             feedback = "correct" if is_correct else "wrong"
 
 
             print(feedback)
 
-
-
-    # add round result to game history
+    # add round result to quiz history
     history_feedback = f"Round {choose}: You are {feedback}."
 
     # Generate history item and add it to game_history list...
     history_item = f"Round {choose}: You choose {selected_graph} {formula} and you are {feedback}."
-    game_history.append(history_item)
+    quiz_history.append(history_item)
 
 
-    keep_going = yes_no("Do you want to continue?")
+
+    # ask user if they want break/continue
+    keep_going = string_checker("Do you want to continue?")
     print()
     if keep_going == "no":
         break
 
+    # keep the round number can not only same number
     choose += 1
 
 print()
@@ -208,16 +157,17 @@ print()
 if choose > 0:
 
     # Display the game history on request
-    see_history = yes_no("Do you want to see your game history? ")
+    see_history = string_checker("Do you want to see your game history? ")
 
+    # check user if they want to see history or not
     if see_history == "yes":
 
-        for item in game_history:
+        for item in quiz_history:
             print(item)
 
 
     print()
-    print(" Thank you use the perimeter and area calculation!!! ")
+    print(" Thank you use the perimeter and area quiz!!! ")
 
 
 
